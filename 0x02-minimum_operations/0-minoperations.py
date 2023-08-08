902b705d96ba56d051dbdcd03e3731e7dab9714d
fort2020 <frankfortdata@gmail.com>
2023-08-08 20:36:06 +0000

    Minimum Operation Required

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""a method that calculates fewest number of operations
+"""a method that calculates the
+   fewest number of operations
 """
 
 
-def minOperations(n: int) -> int:
-    """minimum Operations needed to get n H characters"""
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+def minOperations(n):
+    """
+    Method for compute the minimum number
+    of operations for task Copy All and Paste
+    """
+    if n < 2:
         return 0
-    return op
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
